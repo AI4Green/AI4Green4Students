a5acd3bbf7d4cbd94bb44d03bcd917182bd10469
Shason Gurung <S.Gurung@nottingham.ac.uk>
2023-11-24 13:39:57 +0000

    Merged PR 10180: Bicep update - new connection string
    - New connection string that will point to ai4green db.
    
    Related work items: #135437, #135446, #135447, #135448

--- app/client-app/src/api/section.js
+++ app/client-app/src/api/section.js
@@ -2,51 +2,35 @@ import { useBackendApi } from "contexts/BackendApi";
 import useSWR from "swr";
 
 export const fetchKeys = {
-  sectionsList: (projectId, experimentId) =>
-    `sections?projectId=${projectId}&experimentId=${experimentId}`, // get experiment plan sections of the project and other information based on experimentId
+  sectionsList: (experimentId) =>
+    `sections/listSectionSummaries?experimentId=${experimentId}`, // get experiment plan sections of the project and other information based on experimentId
+  section: (sectionId, experimentId) =>
+    `sections/getSection?sectionId=${sectionId}?experimentId=${experimentId}`, // get section information for a given sectionId
 };
 
-export const useSectionsList = (projectId, experimentId) => {
+export const useSectionsList = (experimentId) => {
   const { apiFetcher } = useBackendApi();
 
   return useSWR(
-    projectId && experimentId
-      ? fetchKeys.sectionsList(projectId, experimentId)
+    experimentId ? fetchKeys.sectionsList(experimentId) : null,
+    async (url) => {
+      const data = await apiFetcher(url);
+      return data;
+    },
+    { suspense: true }
+  );
+};
+
+export const useSection = (sectionId, experimentId) => {
+  const { apiFetcher } = useBackendApi();
+
+  return useSWR(
+    sectionId && experimentId
+      ? fetchKeys.section(sectionId, experimentId)
       : null,
     async (url) => {
       const data = await apiFetcher(url);
-      return [
-        { id: 1, name: "Main", approved: false, comments: 0, sortOrder: 0 },
-        {
-          id: 2,
-          name: "Reaction Scheme",
-          approved: false,
-          comments: 0,
-          sortOrder: 0,
-        },
-        {
-          id: 3,
-          name: "Literature Review",
-          approved: false,
-          comments: 0,
-          sortOrder: 0,
-        },
-        { id: 4, name: "COSH", approved: false, comments: 0, sortOrder: 0 },
-        {
-          id: 5,
-          name: "Safety Data",
-          approved: false,
-          comments: 0,
-          sortOrder: 0,
-        },
-        {
-          id: 6,
-          name: "Experimental Procedure",
-          approved: false,
-          comments: 0,
-          sortOrder: 0,
-        },
-      ];
+      return data;
     },
     { suspense: true }
   );
