f95ed805f6c23564b83fa47058da11433b4f45ab
Philip Van Krimpen <Philip.VanKrimpen@nottingham.ac.uk>
2024-02-21 12:19:32 +0000

    Merged PR 10670: Added SMPT email functionality and updated bicep files
    Adds another email service implementation (ported from biobanks, but verified to be working!)
    
    Bicep files also have the config properties - verified to be working
    
    Related work items: #144669

--- app/AI4Green4Students/Config/EmailSenderOptions.cs
+++ app/AI4Green4Students/Config/EmailSenderOptions.cs
@@ -23,4 +23,14 @@ namespace AI4Green4Students.Config
   {
     public string SendGridApiKey { get; init; } = string.Empty;
   }
+
+  public record SmtpOptions : BaseEmailSenderOptions
+  {
+    public string SmtpHost { get; init; } = string.Empty;
+    public int SmtpPort { get; init; }
+    public int SmtpSecureSocketEnum { get; init; }
+
+    public string SmtpUsername { get; init; } = string.Empty;
+    public string SmtpPassword { get; init; } = string.Empty;
+  }
 }
