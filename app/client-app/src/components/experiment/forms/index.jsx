f8561740aa9cdef74a48b199fb8551e5389b1899
pete_wilson <pete.wilson.000@gmail.com>
2024-05-15 12:13:32 +0100

    Add multi reaction scheme component into report "experimental" section

--- app/client-app/src/components/experiment/forms/index.jsx
+++ app/client-app/src/components/experiment/forms/index.jsx
@@ -4,3 +4,4 @@ export * from "./productYield/ProductYieldTable";
 export * from "./projectGroupActivities/groupPlan/GroupPlanTable";
 export * from "./projectGroupActivities/hazardSummary/HazardSummaryTable";
 export * from "./reactionScheme/ReactionScheme";
+export * from "./reactionScheme/multi/MultiReactionScheme";
