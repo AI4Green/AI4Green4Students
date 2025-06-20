a0dbc2fb7498f2e7f771be5ba6f7afd161416286
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-06-20 19:03:36 +0100

    new generic save method to section form
    - some formatting clean-ups

--- app/AI4Green4Students/Services/SectionFormService.cs
+++ app/AI4Green4Students/Services/SectionFormService.cs
@@ -1,58 +1,71 @@
-using System.Text.Json;
-using AI4Green4Students.Constants;
-using AI4Green4Students.Data.Entities;
-using AI4Green4Students.Models.Field;
-using AI4Green4Students.Models.Section;
-using AI4Green4Students.Utilities;
-
 namespace AI4Green4Students.Services;
+
+using System.Text.Json;
+using Constants;
+using Data;
+using Data.Entities;
+using Data.Entities.SectionTypeData;
+using Microsoft.EntityFrameworkCore;
+using Models.Field;
+using Models.Section;
+using Utilities;
 
 /// <summary>
 /// Contains methods to handle and manage section forms.
 /// </summary>
 public class SectionFormService
 {
-  private readonly SectionService _sections;
+  private readonly ApplicationDbContext _db;
+  private readonly FieldResponseService _fieldResponses;
   private readonly FieldService _fields;
+  private readonly SectionService _sections;
+  private readonly StageService _stages;
 
-  public SectionFormService(SectionService sections, FieldService fields)
+  public SectionFormService(
+    ApplicationDbContext db,
+    SectionService sections,
+    FieldService fields,
+    FieldResponseService fieldResponses,
+    StageService stages
+  )
   {
+    _db = db;
     _sections = sections;
     _fields = fields;
+    _fieldResponses = fieldResponses;
+    _stages = stages;
   }
-  
+
   /// <summary>
-  /// Generate sections summary for a given section type entity.
+  /// Generate section's summary for a given section type entity.
   /// </summary>
-  /// <param name="projectId">Project id</param>
-  /// <param name="sectionType">Section type</param>
-  /// <param name="fieldsResponses">Field responses</param>
-  /// <param name="permissions">Permissions list</param>
-  /// <param name="stage">Stage name</param>
-  /// <returns>Summary model</returns>
-  public async Task<List<SectionSummaryModel>> GetSummaryModel(
-    int projectId,
-    string sectionType,
-    List<FieldResponse> fieldsResponses,
-    List<string> permissions,
-    string stage)
+  /// <param name="id">Entity id.</param>
+  /// <returns>Section summaries.</returns>
+  public async Task<List<SectionSummaryModel>> ListSummary<T>(int id) where T : CoreSectionTypeData
   {
-    var sections = await _sections.ListBySectionTypeName(sectionType, projectId);
-    
+    var sectionType = SectionTypeHelper.GetSectionTypeName<T>();
+    var entity = await GetEntity<T>(id);
+    var fieldsResponses = await _fieldResponses.ListBySectionType<T>(id);
+    var sections = await _sections.ListBySectionTypeName(sectionType, entity.Project.Id);
+
+    // if field has a trigger target, map child field id to parent field
     var triggerMap = new Dictionary<int, Field>();
     fieldsResponses.ForEach(fr =>
     {
-      if (fr.Field.TriggerTarget is not null) // if field has a trigger target, map child field id to parent field
-        triggerMap[fr.Field.TriggerTarget.Id] = fr.Field; 
+      if (fr.Field.TriggerTarget is not null)
+      {
+        triggerMap[fr.Field.TriggerTarget.Id] = fr.Field;
+      }
     });
 
+    var permissions = await _stages.ListPermissions(entity.Stage.SortOrder, sectionType);
     var summaries = sections.Select(section =>
       {
         // get valid field responses for the section.
         // e.g. ignore field responses that are not triggered by parent field
         // useful when determining if a section is approved or not
         var validFieldResponses = fieldsResponses
-          .Where(fr => fr.Field.Section.Id == section.Id && IsFieldTriggeredByParentField(fr.Field, triggerMap));
+          .Where(fr => fr.Field.Section.Id == section.Id && IsFieldTriggeredByParentField(fr.Field.Id, triggerMap));
 
         var fieldResponses = validFieldResponses.ToList();
         return new SectionSummaryModel
@@ -65,98 +78,177 @@ public class SectionFormService
             .Sum(x => x.Conversation.Count(comment => !comment.Read)),
           SortOrder = section.SortOrder,
           SectionType = section.SectionType,
-          Stage = stage,
+          Stage = entity.Stage.Value,
           Permissions = permissions
         };
       })
       .OrderBy(o => o.SortOrder)
       .ToList();
-    
+
     return summaries;
   }
-  
+
   /// <summary>
   /// Generate a form model for a given section.
   /// </summary>
-  /// <param name="sectionId">Section id</param>
-  /// <param name="fieldsResponses">Field responses</param>
-  /// <returns>Form model</returns>
-  public async Task<SectionFormModel> GetFormModel(int sectionId, List<FieldResponse> fieldsResponses)
+  /// <param name="id">Section type entity id. E.g. plan id.</param>
+  /// <param name="sectionId">The section id to generate the form for.</param>
+  /// <returns>Section form.</returns>
+  public async Task<SectionFormModel> GetSectionForm<T>(int id, int sectionId) where T : CoreSectionTypeData
   {
+    var fieldsResponses = await _fieldResponses.ListBySection<T>(id, sectionId);
     var section = await _sections.Get(sectionId);
     var sectionFields = await _fields.ListBySection(sectionId);
-    
+
     return new SectionFormModel
     {
       Id = section.Id,
       Name = section.Name,
       FieldResponses = sectionFields.Select(x => new FieldResponseFormModel
-      {
-        Id = x.Id,
-        Name = x.Name,
-        Mandatory = x.Mandatory,
-        Hidden = x.Hidden,
-        SortOrder = x.SortOrder,
-        FieldType = x.InputType.Name,
-        DefaultResponse = x.DefaultResponse,
-        SelectFieldOptions = x.SelectFieldOptions.Count >= 1
-          ? x.SelectFieldOptions
-            .Select(option => new SelectFieldOptionModel(option))
-            .ToList()
-          : null,
-        Trigger = (x.TriggerCause != null && x.TriggerTarget != null)
-          ? new TriggerFormModel
-          {
-            Value = x.TriggerCause,
-            Target = x.TriggerTarget.Id
-          }
-          : null,
-        FieldResponseId = fieldsResponses.FirstOrDefault(y => y.Field.Id == x.Id)?.Id,
-        FieldResponse = SerializerHelper.DeserializeOrDefault<JsonElement>(
-          // direct deserialisation should work as we expect Value to be always a valid json string,
-          // but just to ensure we correctly handle invalid json strings
-          fieldsResponses
+        {
+          Id = x.Id,
+          Name = x.Name,
+          Mandatory = x.Mandatory,
+          Hidden = x.Hidden,
+          SortOrder = x.SortOrder,
+          FieldType = x.InputType.Name,
+          DefaultResponse = x.DefaultResponse,
+          SelectFieldOptions = x.SelectFieldOptions.Count >= 1
+            ? x.SelectFieldOptions.Select(option => new SelectFieldOptionModel(option)).ToList()
+            : null,
+          Trigger = x.TriggerCause != null && x.TriggerTarget != null
+            ? new TriggerFormModel
+            {
+              Value = x.TriggerCause,
+              Target = x.TriggerTarget.Id
+            }
+            : null,
+          FieldResponseId = fieldsResponses.FirstOrDefault(y => y.Field.Id == x.Id)?.Id,
+          FieldResponse = SerializerHelper.DeserializeOrDefault<JsonElement>(
+            // direct deserialisation should work as we expect Value to be always a valid JSON string,
+            // but just to ensure we correctly handle invalid JSON strings
+            fieldsResponses
+              .Where(y => y.Field.Id == x.Id)
+              .Select(y => y.FieldResponseValues.MaxBy(z => z.ResponseDate)?.Value)
+              .SingleOrDefault() ?? JsonSerializer.Serialize(x.DefaultResponse)
+          ), // default response if no response found
+          IsApproved = fieldsResponses.Any(y => y.Field.Id == x.Id && y.Approved),
+          Comments = fieldsResponses
             .Where(y => y.Field.Id == x.Id)
-            .Select(y => y.FieldResponseValues.MaxBy(z => z.ResponseDate)?.Value)
-            .SingleOrDefault() ?? JsonSerializer.Serialize(x.DefaultResponse)), // default response if no response found
-        IsApproved = fieldsResponses.Any(y => y.Field.Id == x.Id && y.Approved),
-        Comments = fieldsResponses
-          .Where(y => y.Field.Id == x.Id)
-          .Sum(y => y.Conversation.Count),
-        UnreadComments = fieldsResponses
-          .Where(y => y.Field.Id == x.Id)
-          .Sum(y => y.Conversation.Count(comment => !comment.Read)),
-      }).ToList()
+            .Sum(y => y.Conversation.Count),
+          UnreadComments = fieldsResponses
+            .Where(y => y.Field.Id == x.Id)
+            .Sum(y => y.Conversation.Count(comment => !comment.Read))
+        })
+        .ToList()
     };
   }
-  
+
+  /// <summary>
+  /// Save the section form data.
+  /// </summary>
+  /// <param name="model">Payload to save.</param>
+  /// <returns>Section form.</returns>
+  public async Task<SectionFormModel> SaveForm<T>(SectionFormPayloadModel model) where T : CoreSectionTypeData
+  {
+    // Transform the payload model to a submission model.
+    // Basically, we are preparing the data to be saved in the database.
+    var submission = new SectionFormSubmissionModel
+    {
+      SectionId = model.SectionId,
+      RecordId = model.RecordId,
+      FieldResponses = await _fieldResponses.GenerateFieldResponseSubmissionModel(
+        model.FieldResponses,
+        model.Files,
+        model.FileFieldResponses
+      ),
+      NewFieldResponses = await _fieldResponses.GenerateFieldResponseSubmissionModel(
+        model.NewFieldResponses,
+        model.NewFiles,
+        model.NewFileFieldResponses,
+        true
+      )
+    };
+
+    var existing = await GetEntity<T>(model.RecordId);
+    var fieldResponses = await _fieldResponses.ListBySection<T>(submission.RecordId, submission.SectionId);
+
+    var updatedValues =
+      existing.Stage.Value == Stages.Draft
+        ? _fieldResponses.UpdateDraft(submission.FieldResponses, fieldResponses)
+        : _fieldResponses.UpdateAwaitingChanges(submission.FieldResponses, fieldResponses);
+
+    foreach (var updatedValue in updatedValues)
+    {
+      _db.Update(updatedValue);
+    }
+    await _db.SaveChangesAsync();
+
+    if (submission.NewFieldResponses.Count == 0)
+    {
+      return await GetSectionForm<T>(model.RecordId, model.SectionId);
+    }
+
+    var newFieldResponses = await _fieldResponses.CreateResponses<T>(
+      existing.Id,
+      existing.Project.Id,
+      SectionTypeHelper.GetSectionTypeName<T>(),
+      submission.NewFieldResponses
+    );
+
+    existing.FieldResponses.AddRange(newFieldResponses);
+    await _db.SaveChangesAsync();
+
+    return await GetSectionForm<T>(submission.RecordId, submission.SectionId);
+  }
+
+  /// <summary>
+  /// Get entity by id.
+  /// </summary>
+  /// <param name="id">Entity id.</param>
+  /// <returns>Entity.</returns>
+  private async Task<T> GetEntity<T>(int id) where T : CoreSectionTypeData
+    => await _db.Set<T>()
+      .Where(x => x.Id == id)
+      .Include(x => x.Project)
+      .Include(x => x.Owner)
+      .Include(x => x.Stage)
+      .SingleOrDefaultAsync() ?? throw new KeyNotFoundException();
+
   /// <summary>
   /// Helper method to check if a field is triggered by a parent field.
   /// </summary>
-  /// <param name="field">Field to check</param>
-  /// <param name="childFieldsAndParentFields">Dictionary of child fields and their parent fields</param>
+  /// <param name="id">Field id to check</param>
+  /// <param name="childToParentMap">Dictionary of child field id to parent field</param>
   /// <returns>Bool</returns>
-  private bool IsFieldTriggeredByParentField(Field field, Dictionary<int, Field> childFieldsAndParentFields)
+  private static bool IsFieldTriggeredByParentField(int id, Dictionary<int, Field> childToParentMap)
   {
-    if (!childFieldsAndParentFields.TryGetValue(field.Id, out Field? parentField)) return true;
-    var parentFieldResponse = parentField.FieldResponses
-      .Select(x => x.FieldResponseValues.MaxBy(y => y.ResponseDate)?.Value)
+    if (!childToParentMap.TryGetValue(id, out var parentField))
+    {
+      return true;
+    }
+    var parentFieldResponse = parentField
+      .FieldResponses.Select(x => x.FieldResponseValues.MaxBy(y => y.ResponseDate)?.Value)
       .SingleOrDefault();
 
-    // we are checking whether parent field response value is equal to the trigger cause.
-    // since field response value is always a json string, we need to deserialise it to the correct type before comparison
+    // we are checking whether a parent field response value is equal to the trigger cause.
+    // since field response value is always a JSON string,
+    // we need to deserialise it to the correct type before comparison
     switch (parentField.InputType.Name)
     {
       case InputTypes.Multiple:
       case InputTypes.Radio:
-        return parentFieldResponse is not null && 
-               (SerializerHelper.DeserializeOrDefault<List<SelectFieldOptionModel>>(parentFieldResponse)?
-                 .Any(x => x.Name == parentField.TriggerCause) ?? false);
+        return parentFieldResponse is not null
+               && (
+                 SerializerHelper
+                   .DeserializeOrDefault<List<SelectFieldOptionModel>>(parentFieldResponse)
+                   ?.Any(x => x.Name == parentField.TriggerCause) ?? false
+               );
 
       default:
-        // shouldn't reach here as we expect only select (Radio and Multiple) fields to have trigger cause.
-        return parentFieldResponse is not null &&
-               SerializerHelper.DeserializeOrDefault<string>(parentFieldResponse) == parentField.TriggerCause;
+        // shouldn't reach here as we expect only a select (Radio and Multiple) fields to have trigger cause.
+        return parentFieldResponse is not null
+               && SerializerHelper.DeserializeOrDefault<string>(parentFieldResponse) == parentField.TriggerCause;
     }
   }
 }
