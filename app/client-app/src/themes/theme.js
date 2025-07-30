36d9e25a2260f44fe2dae6eb006d823039d0c17b
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-07-30 12:57:20 +0100

    improve layout

--- app/client-app/src/themes/theme.js
+++ app/client-app/src/themes/theme.js
@@ -66,6 +66,10 @@ export const theme = extendTheme({
     "4xl": "2.25rem",
     "5xl": "3rem",
     "6xl": "4rem",
+    "7xl": "5rem",
+    "8xl": "6rem",
+    "9xl": "7rem",
+    "10xl": "8rem",
   },
   components: {
     Button: {
