c79b16164af047fbe447c8baf17ee36ecd642692
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-12 21:50:23 +0100

    Increases request timeout
    This change prevents premature request termination, especially for
    slower or more complex backend operations.

--- app/client-app/src/contexts/backend-api.jsx
+++ app/client-app/src/contexts/backend-api.jsx
@@ -27,7 +27,7 @@ export const getBackendDefaults = (language) => ({
   headers: {
     "Accept-Language": language,
   },
-  timeout: 0.5 * 60 * 1000,
+  timeout: 5 * 60 * 1000,
 });
 
 export const BackendApiProvider = ({ children }) => {
