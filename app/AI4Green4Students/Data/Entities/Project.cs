919fc98f744ccffe5ab926c7037fef5f7a4efe42
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-14 14:52:23 +0100

    Reference ProjectType entity

--- app/AI4Green4Students/Data/Entities/Project.cs
+++ app/AI4Green4Students/Data/Entities/Project.cs
@@ -6,11 +6,11 @@ namespace AI4Green4Students.Data.Entities;
 public class Project
 {
   public int Id { get; set; }
-  public string Name { get; set; } = string.Empty;
-  public List<ProjectGroup> ProjectGroups { get; set; } = null!;
-  public List<Plan> Plans { get; set; } = null!;
-  public List<Note> Notes { get; set; } = null!;
-  public List<Report> Reports { get; set; } = null!;
-  public List<Section> Sections { get; set; } = null!;
-  public List<ApplicationUser> Instructors { get; set; } = new();
+  public required string Name { get; set; }
+  public List<ProjectGroup> ProjectGroups { get; set; } = new  List<ProjectGroup>();
+  public List<Plan> Plans { get; set; } = new List<Plan>();
+  public List<Note> Notes { get; set; } = new List<Note>();
+  public List<Report> Reports { get; set; } = new List<Report>();
+  public List<ApplicationUser> Instructors { get; set; } = new List<ApplicationUser>();
+  public ProjectType? ProjectType { get; set; }
 }
--- app/AI4Green4Students/Data/Entities/Section.cs
+++ app/AI4Green4Students/Data/Entities/Section.cs
@@ -1,16 +1,14 @@
-using Microsoft.Identity.Client;
-
 namespace AI4Green4Students.Data.Entities;
 
 /// <summary>
-/// A section splits up the fields for an experiment. Each section can be approved by the instructor.
+/// Represents a section within a section type (e.g. Plan or Lab-Note).
 /// </summary>
 public class Section
 {
   public int Id { get; set; }
-  public string Name { get; set; } = string.Empty;
-  public Project Project { get; set; } = null!;
-  public List<Field> Fields { get; set; } = null!;
+  public required string Name { get; set; }
+  public ProjectType? ProjectType { get; set; }
+  public int SortOrder { get; set; }
+  public List<Field> Fields { get; set; } = new List<Field>();
   public SectionType SectionType { get; set; } = null!;
-  public int SortOrder { get; set; } = 0;
 }
