96210c42dd911a88c01f5ca0ada05f90c31e7c12
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-14 17:44:46 +0100

    Add PredictorOptions configuration and update appsettings

---
+++ app/AI4Green4Students/Config/PredictorOptions.cs
@@ -0,0 +1,7 @@
+namespace AI4Green4Students.Config;
+
+public class PredictorOptions
+{
+  public string ForwardPredictionApiUrl { get; set; } = string.Empty;
+  public string ForwardPredictionApiKey { get; set; } = string.Empty;
+}
