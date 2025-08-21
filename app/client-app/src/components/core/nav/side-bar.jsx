96956464844fc375ccfd62b20a442de934688506
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-21 17:42:11 +0100

    Refactor sidebar component to correctly display sidebar items

--- app/client-app/src/components/core/nav/side-bar.jsx
+++ app/client-app/src/components/core/nav/side-bar.jsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Divider,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
@@ -7,11 +8,14 @@ import {
   DrawerOverlay,
   Grid,
   Heading,
+  HStack,
   IconButton,
+  useBreakpointValue,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react";
 import { NavBar, SidebarButton } from "components/core/nav";
+import { navbarItems } from "config/navbar-items";
 import { getSidebarItems } from "config/sidebar-items";
 import { useUser } from "contexts";
 import { useTranslation } from "react-i18next";
@@ -21,20 +25,7 @@ import { Link } from "react-router-dom";
 export const Sidebar = ({ children }) => {
   const { t } = useTranslation();
   const { user } = useUser();
-
-  const BrandLink = () => (
-    <Link to="/">
-      <Heading
-        as="h1"
-        fontSize={{ base: "md", sm: "lg", md: "xl", lg: "2xl" }}
-        fontWeight="light"
-        letterSpacing="tighter"
-        color="brand.500"
-      >
-        {t("buttons.brand")}
-      </Heading>
-    </Link>
-  );
+  const isFullMenu = useBreakpointValue({ base: false, xl: true });
 
   // filter items that the user has permission to access or where no permission is required
   const validItems = getSidebarItems(t).filter(
@@ -46,49 +37,42 @@ export const Sidebar = ({ children }) => {
   );
 
   // If there are no valid items, don't render the sidebar
-  return validItems.length > 0 ? (
-    <VStack overflow="auto" w="100%">
-      <Grid templateRows="auto 1fr" minW="100%">
-        <NavBar
-          brand={
-            <>
-              <Box
-                display="none"
-                h="screen"
-                w="full"
-                flexShrink={0}
-                borderRightWidth={1}
-                borderRightColor="gray.200"
-              >
-                <Box h="full" pt={4} pb={6} lg={{ pt: 4, pb: 8 }}>
-                  <SidebarContent brand={<BrandLink />} items={validItems} />
-                </Box>
-              </Box>
-              <SideMenuDrawer items={validItems} brand={<BrandLink />} />
-            </>
-          }
-        />
-        {children}
-      </Grid>
-    </VStack>
+  return validItems.length || (navbarItems.length > 0 && !isFullMenu) ? (
+    <Grid templateRows="auto 1fr" minW="100%">
+      <NavBar
+        brand={
+          <HStack spacing={2}>
+            <SideMenuDrawer
+              items={validItems}
+              brand={
+                <BrandLink
+                  fontSize={{ base: "2xl", sm: "3xl" }}
+                  fontWeight="hairline"
+                />
+              }
+              isFullMenu={isFullMenu}
+            />
+            <BrandLink />
+          </HStack>
+        }
+      />
+      {children}
+    </Grid>
   ) : (
-    <VStack overflow="auto" w="100%">
-      <Grid templateRows="auto 1fr" minW="100%">
-        <NavBar brand={<BrandLink />} />
-        {children}
-      </Grid>
-    </VStack>
+    <Grid templateRows="auto 1fr" minW="100%">
+      <NavBar brand={<BrandLink />} />
+      {children}
+    </Grid>
   );
 };
 
-const SideMenuDrawer = ({ items, brand }) => {
-  // only applicable to small screens
+const SideMenuDrawer = ({ items, brand, isFullMenu }) => {
   const DrawerState = useDisclosure();
   return (
-    <Box ml={1} mt={1}>
+    <>
       <IconButton
-        icon={<FaBars />}
-        size="lg"
+        icon={<FaBars size={20} />}
+        size="xs"
         onClick={DrawerState.onOpen}
         variant="ghost"
         aria-label="Open sidebar menu"
@@ -103,24 +87,59 @@ const SideMenuDrawer = ({ items, brand }) => {
         <DrawerContent>
           <DrawerCloseButton aria-label="Close sidebar menu" />
           <DrawerBody>
-            <SidebarContent
-              items={items}
-              brand={brand}
-              onClose={DrawerState.onClose}
-            />
+            <VStack spacing={8} align="stretch" as="aside">
+              <VStack spacing={4} mt={4} align="stretch">
+                {brand}
+                <Divider borderColor="brand.200" />
+              </VStack>
+
+              {items.length > 0 && (
+                <VStack spacing={4}>
+                  {items.map((item, i) => (
+                    <SidebarButton
+                      item={item}
+                      key={i}
+                      onClose={DrawerState.onClose}
+                    />
+                  ))}
+                  <Divider borderColor="brand.100" />
+                </VStack>
+              )}
+
+              {!isFullMenu && navbarItems.length > 0 && (
+                <VStack spacing={4}>
+                  {navbarItems.map((item, i) => (
+                    <SidebarButton
+                      item={item}
+                      key={i}
+                      onClose={DrawerState.onClose}
+                    />
+                  ))}
+                  <Divider borderColor="brand.100" />
+                </VStack>
+              )}
+            </VStack>
           </DrawerBody>
         </DrawerContent>
       </Drawer>
-    </Box>
+    </>
   );
 };
 
-const SidebarContent = ({ brand, items, onClose }) => (
-  <VStack spacing={5} align="flex-start" px={4} as="aside">
-    {brand}
-
-    {items?.map((item, i) => (
-      <SidebarButton item={item} key={i} onClose={onClose} />
-    ))}
-  </VStack>
-);
+const BrandLink = ({ ...p }) => {
+  const { t } = useTranslation();
+  return (
+    <Link to="/">
+      <Heading
+        as="h1"
+        fontSize={{ base: "xl", sm: "2xl" }}
+        fontWeight="light"
+        letterSpacing="tighter"
+        color="brand.500"
+        {...p}
+      >
+        {t("buttons.brand")}
+      </Heading>
+    </Link>
+  );
+};
