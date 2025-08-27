7ae970a5b6b611d865c794e1a7d2f83931163d2b
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-27 16:11:48 +0100

    AB#219037: Display project type description in a tooltip (#13)

--- app/client-app/src/components/core/forms/multi-select-field.jsx
+++ app/client-app/src/components/core/forms/multi-select-field.jsx
@@ -2,77 +2,21 @@
   Select component, which can be used as either single select or multi select
 */
 
-import { FormControl, FormLabel, theme } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  HStack,
+  Icon,
+  Text,
+  theme,
+  Tooltip,
+} from "@chakra-ui/react";
 import { Field } from "formik";
+import { IoMdInformationCircleOutline } from "react-icons/io";
 import Select from "react-select";
 
 import { FormHelpError } from "./form-help-error";
-
-const CustomSelect = ({
-  field,
-  form,
-  isMulti = false,
-  options,
-  label,
-  placeholder,
-  isDisabled,
-}) => {
-  const onChange = (option) => {
-    const selectedValue = isMulti
-      ? option.map((item) => item.value)
-      : [option?.value || ""];
-
-    form.setFieldValue(field.name, selectedValue);
-  };
-
-  const selected = () => {
-    return options
-      ? options.filter((option) => field.value.indexOf(option.value) >= 0)
-      : [];
-  };
-
-  const styles = {
-    control: (baseStyles, state) => ({
-      ...baseStyles,
-      border: state.isFocused
-        ? `1px Solid ${theme.colors.blue[800]}`
-        : selected().length < 1 &&
-          !state.isFocused &&
-          `2px Solid ${theme.colors.red[500]}`,
-      "&:hover": {
-        backgroundColor: theme.colors.gray[100],
-      },
-    }),
-
-    menuPortal: (baseStyles) => ({
-      ...baseStyles,
-      zIndex: 9999,
-    }),
-  };
-
-  return (
-    <FormControl isRequired isInvalid={form.errors[field.name]}>
-      <FormLabel>{label}</FormLabel>
-      <Select
-        name={field.name}
-        value={selected()}
-        onChange={onChange}
-        placeholder={placeholder}
-        options={options}
-        isMulti={isMulti}
-        styles={styles}
-        isDisabled={isDisabled}
-        menuPortalTarget={document.body}
-        menuPosition="fixed"
-      />
-      <FormHelpError
-        isInvalid={form.errors[field.name] && form.touched[field.name]}
-        error={form.errors[field.name]}
-        collapseEmpty
-      />
-    </FormControl>
-  );
-};
 
 export const MultiSelectField = ({
   options,
@@ -80,22 +24,75 @@ export const MultiSelectField = ({
   placeholder,
   label,
   isMulti,
-  initialValue, // initially selected values
+  initialValue,
   isDisabled,
-}) => {
+}) => (
+  <Field name={name} initialValue={initialValue}>
+    {({ field, form }) => {
+      const onChange = (option) => {
+        const selectedValue = isMulti
+          ? option.map((item) => item.value)
+          : [option?.value || ""];
+        form.setFieldValue(field.name, selectedValue);
+      };
+
+      const selected = () => {
+        return options
+          ? options.filter((option) => field.value.indexOf(option.value) >= 0)
+          : [];
+      };
+
+      return (
+        <FormControl isRequired isInvalid={form.errors[field.name]}>
+          <FormLabel>{label}</FormLabel>
+          <Select
+            name={field.name}
+            value={selected()}
+            onChange={onChange}
+            placeholder={placeholder}
+            options={options}
+            isMulti={isMulti}
+            isDisabled={isDisabled}
+            components={{ Option: CustomOption }}
+          />
+          <FormHelpError
+            isInvalid={form.errors[field.name] && form.touched[field.name]}
+            error={form.errors[field.name]}
+            collapseEmpty
+          />
+        </FormControl>
+      );
+    }}
+  </Field>
+);
+
+const CustomOption = ({ data, ...props }) => {
   return (
-    <Field name={name} initialValue={initialValue}>
-      {({ field, form }) => (
-        <CustomSelect
-          field={field}
-          form={form}
-          isMulti={isMulti}
-          options={options}
-          label={label}
-          placeholder={placeholder}
-          isDisabled={isDisabled}
-        />
-      )}
-    </Field>
+    <Box {...props.innerProps} _hover={{ bg: theme.colors.gray[100] }}>
+      <HStack
+        fontSize="sm"
+        fontWeight="thin"
+        py={1}
+        px={2}
+        justify="space-between"
+      >
+        <Text>{data.label}</Text>
+        {data.description && (
+          <Tooltip
+            label={data.description}
+            placement="right"
+            hasArrow
+            fontSize="xs"
+          >
+            <Icon
+              as={IoMdInformationCircleOutline}
+              ml={2}
+              boxSize={4}
+              cursor="help"
+            />
+          </Tooltip>
+        )}
+      </HStack>
+    </Box>
   );
 };
