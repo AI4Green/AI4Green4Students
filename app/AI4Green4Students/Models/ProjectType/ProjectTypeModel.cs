bc4d43e5d87535044e0f05dea4622cfc6ab3b219
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-08-29 20:08:35 +0100

    add stage in the model

--- app/AI4Green4Students/Models/ProjectType/ProjectTypeModel.cs
+++ app/AI4Green4Students/Models/ProjectType/ProjectTypeModel.cs
@@ -3,13 +3,15 @@ namespace AI4Green4Students.Models.ProjectType;
 public record ProjectTypeModel(
   int Id,
   string Name,
-  string Description
+  string Description,
+  string Stage
 )
 {
   public ProjectTypeModel(Data.Entities.ProjectType entity) : this(
     entity.Id,
     entity.Name,
-    entity.Description
+    entity.Description,
+    entity.Stage.DisplayName
   )
   {
   }
