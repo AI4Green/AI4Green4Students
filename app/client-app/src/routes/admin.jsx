ccfd6329402fe4db554c149bee2136d659334186
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-09-02 00:31:22 +0100

    project type sections setup

--- app/client-app/src/routes/admin.jsx
+++ app/client-app/src/routes/admin.jsx
@@ -7,6 +7,7 @@ import { ProtectedRoutes } from "layouts/protected-routes";
 import { ProjectTypeManagement } from "pages/admin/project-type-management";
 import { UserManagement } from "pages/admin/user-management";
 import { NotFound } from "pages/error";
+import { ProjectTypeView } from "pages/project-type/view";
 import { RegistrationRule } from "pages/registration-rule";
 import { Route, Routes } from "react-router-dom";
 
@@ -56,6 +57,29 @@ export const Admin = () => (
       }
     >
       <Route index element={<ProjectTypeManagement />} />
+    </Route>
+
+    <Route
+      path="project-type-management/:projectTypeId"
+      element={
+        <ProtectedRoutes
+          isAuthorized={(user) =>
+            Object.values(PROJECT_TYPE_MANAGEMENT_PERMISSIONS).every(
+              (permission) => user.permissions?.includes(permission)
+            )
+          }
+        />
+      }
+    >
+      <Route index element={<ProjectTypeView />} />
+      <Route
+        path="section-types/:sectionTypeId/sections"
+        element={<ProjectTypeView />}
+      />
+      <Route
+        path="section-types/:sectionTypeId/sections/:sectionId/fields"
+        element={<ProjectTypeView />}
+      />
     </Route>
 
     <Route path="*" element={<NotFound />} />
