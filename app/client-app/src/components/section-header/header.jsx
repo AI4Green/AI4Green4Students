08c6698fb084a415aae9c650ff79c7a34abdff3e
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-09-08 01:48:34 +0100

    refactor header component

--- app/client-app/src/components/section-header/header.jsx
+++ app/client-app/src/components/section-header/header.jsx
@@ -1,63 +1,53 @@
-import { Avatar, Heading, HStack, Icon, Text, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import { Badge } from "components/core/Badge";
 import { TITLE_ICON_COMPONENTS } from "constants";
 
-export const SectionHeader = ({
-  header,
-  project,
-  overviewTitle,
-  owner,
-  icon,
-  actionSection,
-}) => (
+export const SectionHeader = ({ header, project, owner, action }) => (
   <HStack w="full" justify="space-between" borderBottomWidth={1} py={4}>
-    <VStack gap={4} align="start">
-      {header && (
-        <Heading
-          as="h2"
-          fontSize={{ base: "sm", lg: "md" }}
-          fontWeight="normal"
-          color="gray.700"
-        >
-          {header}
-        </Heading>
-      )}
+    <VStack spacing={4} align="start">
+      <HStack spacing={6}>
+        {header.title && (
+          <Heading
+            as="h2"
+            fontSize={{ base: "sm", lg: "md" }}
+            fontWeight="normal"
+            color="gray.700"
+          >
+            {header.title}
+          </Heading>
+        )}
+        <HStack spacing={4}>
+          <Badge
+            label={header.type}
+            colorScheme="green"
+            leftIcon={header.icon}
+            fontSize="xxs"
+          />
+          <Badge
+            label={project.name}
+            leftIcon={TITLE_ICON_COMPONENTS.Project}
+            colorScheme="brand"
+            variant="outline"
+            fontSize="xxs"
+          />
+        </HStack>
+      </HStack>
       <HStack align="center" gap={4}>
         {owner && (
           <HStack>
-            <Avatar name={owner} size="xs" />
+            <Avatar name={owner.name} size="xs" />
             <Text
               fontSize={{ base: "xs", md: "sm" }}
               fontWeight="light"
               color="gray.700"
             >
-              {owner}
+              {owner.name}
             </Text>
           </HStack>
         )}
-
-        <Text
-          fontSize={{ base: "xs", md: "sm" }}
-          color="brand.500"
-          fontWeight="semibold"
-        >
-          <Icon as={TITLE_ICON_COMPONENTS.Project} /> Project - {project.name}
-        </Text>
       </HStack>
     </VStack>
 
-    <VStack align="end" gap={2}>
-      <HStack align="baseline">
-        <Icon as={icon} boxSize="5" color="brand.500" />
-        <Heading
-          as="h1"
-          fontSize={{ base: "md", md: "lg", lg: "xl", "2xl": "2xl" }}
-          fontWeight="normal"
-          color="brand.500"
-        >
-          {overviewTitle}
-        </Heading>
-      </HStack>
-      {actionSection}
-    </VStack>
+    <VStack align="end">{action}</VStack>
   </HStack>
 );
