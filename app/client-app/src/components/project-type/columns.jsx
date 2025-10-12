cd83e1e923f0cfd289ace249107f0827657a9174
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-10-12 17:31:19 +0100

    add project type action to publish, draft and deprecate

--- app/client-app/src/components/project-type/columns.jsx
+++ app/client-app/src/components/project-type/columns.jsx
@@ -1,15 +1,26 @@
-import { Flex, Icon, Text } from "@chakra-ui/react";
+import { Flex, Icon, Text, useDisclosure } from "@chakra-ui/react";
+import { useProjectTypesList } from "api";
 import { ActionButton } from "components/core/action-button";
 import { DataTableColumnHeader } from "components/core/data-table";
 import { DeleteModal } from "components/project-type/modal-delete";
 import { CreateOrEditProjectTypeModal } from "components/project-type/modal-form";
+import { MoveStageModal } from "components/stage/move-stage";
 import {
   PROJECT_TYPE_MANAGEMENT_PERMISSIONS,
+  STAGES,
+  STAGES_PERMISSIONS,
   STATUS_ICON_COMPONENTS,
   TITLE_ICON_COMPONENTS,
 } from "constants";
 import { useUser } from "contexts";
-import { FaCheck, FaLink, FaTimes, FaTrash } from "react-icons/fa";
+import {
+  FaCheck,
+  FaDraftingCompass,
+  FaLink,
+  FaPaperPlane,
+  FaTimes,
+  FaTrash,
+} from "react-icons/fa";
 import { useSearchParams } from "react-router-dom";
 
 export const columns = [
@@ -97,10 +108,7 @@ export const columns = [
       <DataTableColumnHeader column={column} title="Actions" />
     ),
     cell: ({ row }) => (
-      <Action
-        projectTypeId={row.original.id}
-        inUse={row.original.inUseCount > 0}
-      />
+      <Action projectType={row.original} inUse={row.original.inUseCount > 0} />
     ),
     meta: {
       width: 1,
@@ -108,31 +116,89 @@ export const columns = [
   },
 ];
 
-const Action = ({ projectTypeId, inUse }) => {
+const Action = ({ projectType, inUse }) => {
   const { user } = useUser();
   const [searchParams, setSearchParams] = useSearchParams();
   const action = searchParams.get("action");
   const id = searchParams.get("id");
+
+  const {
+    isOpen: isPublishOpen,
+    onOpen: onPublishOpen,
+    onClose: onPublishClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isDraftOpen,
+    onOpen: onDraftOpen,
+    onClose: onDraftClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isDeprecateOpen,
+    onOpen: onDeprecateOpen,
+    onClose: onDeprecateClose,
+  } = useDisclosure();
+
+  const { mutate } = useProjectTypesList();
+  const modalDefaultProps = {
+    record: {
+      id: projectType.id,
+      title: projectType.name,
+    },
+    type: "ProjectType",
+    mutate: mutate,
+  };
 
   const actions = {
     edit: {
       isEligible: () =>
         user.permissions.includes(
           PROJECT_TYPE_MANAGEMENT_PERMISSIONS.EditProjectTypes
-        ),
+        ) && projectType.stage === STAGES.Draft,
       icon: <FaLink />,
       label: "Edit",
-      onClick: () => setSearchParams({ action: "edit", id: projectTypeId }),
+      onClick: () => setSearchParams({ action: "edit", id: projectType.id }),
     },
     delete: {
       isEligible: () =>
         !inUse &&
         user.permissions.includes(
           PROJECT_TYPE_MANAGEMENT_PERMISSIONS.DeleteProjectTypes
-        ),
+        ) &&
+        projectType.stage === STAGES.Draft,
       icon: <FaTrash />,
       label: "Delete",
-      onClick: () => setSearchParams({ action: "delete", id: projectTypeId }),
+      onClick: () => setSearchParams({ action: "delete", id: projectType.id }),
+    },
+    publish: {
+      isEligible: () =>
+        user.permissions.includes(
+          PROJECT_TYPE_MANAGEMENT_PERMISSIONS.EditProjectTypes
+        ) && projectType.permissions.includes(STAGES_PERMISSIONS.CanPublish),
+
+      icon: <FaPaperPlane />,
+      label: "Publish",
+      onClick: onPublishOpen,
+    },
+    draft: {
+      isEligible: () =>
+        user.permissions.includes(
+          PROJECT_TYPE_MANAGEMENT_PERMISSIONS.EditProjectTypes
+        ) && projectType.permissions.includes(STAGES_PERMISSIONS.CanPutInDraft),
+
+      icon: <FaDraftingCompass />,
+      label: "Convert to Draft",
+      onClick: onDraftOpen,
+    },
+    deprecate: {
+      isEligible: () =>
+        user.permissions.includes(
+          PROJECT_TYPE_MANAGEMENT_PERMISSIONS.EditProjectTypes
+        ) && projectType.permissions.includes(STAGES_PERMISSIONS.CanDeprecate),
+      icon: <FaTrash />,
+      label: "Deprecate",
+      onClick: onDeprecateOpen,
     },
   };
   return (
@@ -140,6 +206,42 @@ const Action = ({ projectTypeId, inUse }) => {
       <ActionButton actions={actions} size="xs" />
       {action === "edit" && id && <CreateOrEditProjectTypeModal />}
       {action === "delete" && id && <DeleteModal />}
+
+      {isPublishOpen && (
+        <MoveStageModal
+          fixedNextStage={STAGES.Ready}
+          modalTitle="Publish Project Type"
+          modalMessage="Are you sure you want to publish this project type?"
+          successMessage="Project type published successfully"
+          isModalOpen={isPublishOpen}
+          onModalClose={onPublishClose}
+          {...modalDefaultProps}
+        />
+      )}
+
+      {isDraftOpen && (
+        <MoveStageModal
+          fixedNextStage={STAGES.Draft}
+          modalTitle="Convert to Draft"
+          modalMessage="Are you sure you want to convert this project type to draft?"
+          successMessage="Project type converted to draft successfully"
+          isModalOpen={isDraftOpen}
+          onModalClose={onDraftClose}
+          {...modalDefaultProps}
+        />
+      )}
+
+      {isDeprecateOpen && (
+        <MoveStageModal
+          fixedNextStage={STAGES.Deprecated}
+          modalTitle="Deprecate Project Type"
+          modalMessage="Are you sure you want to deprecate this project type?"
+          successMessage="Project type deprecated successfully"
+          isModalOpen={isDeprecateOpen}
+          onModalClose={onDeprecateClose}
+          {...modalDefaultProps}
+        />
+      )}
     </>
   );
 };
