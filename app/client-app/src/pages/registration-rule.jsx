dce5e08ee36651bba7c40537213e60db6bb6778b
Shason Gurung <s.gurung@nottingham.ac.uk>
2025-11-28 13:42:14 +0000

    updated registration rule page including breadcrumbs

---
+++ app/client-app/src/pages/registration-rule.jsx
@@ -0,0 +1,27 @@
+import { HStack } from "@chakra-ui/react";
+import { Breadcrumbs } from "components/core/breadcrumbs";
+import { Table } from "components/registration-rule/table";
+import { TITLE_ICON_COMPONENTS } from "constants";
+import { DefaultContentHeader, DefaultContentLayout } from "layouts/default";
+
+export const RegistrationRule = () => {
+  const breadcrumbItems = [
+    { label: "Home", href: "/" },
+    {
+      label: "Registration Rule Management",
+    },
+  ];
+
+  return (
+    <DefaultContentLayout>
+      <Breadcrumbs items={breadcrumbItems} />
+      <HStack>
+        <DefaultContentHeader
+          header="Registration Rule Management"
+          icon={TITLE_ICON_COMPONENTS.RegistrationRule}
+        />
+      </HStack>
+      <Table />
+    </DefaultContentLayout>
+  );
+};
